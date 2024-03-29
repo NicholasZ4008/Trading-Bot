d362a22e42a0c2209dd4ad7bdb119ff6cf84c0af
Adam <afb2@sfu.ca>
2024-03-28 22:24:39 -0700

    initial

---
+++ main.py
@@ -0,0 +1,19 @@
+from web_scraper import getting_data, DataFrame_formatting
+from noise_filtering import LOESS_smoothing, Kalman_filtering
+from yahoofinance import getting_gaining_stocks
+from indicator import indicator_for_setup
+import matplotlib.pyplot as plt
+def main():
+    values_list = getting_data()
+    stock_df=DataFrame_formatting(values_list)
+    stock_df.to_csv('dataset.csv', index=False)  
+    LOESS_smoothing('dataset.csv')
+    plt.savefig('filtered_profit_margin_plot.png')
+    plt.clf()
+    Kalman_filtering('dataset.csv')
+    plt.savefig('Volume_traded_true_state.png')
+    plt.clf()
+    getting_gaining_stocks()
+    indicator_for_setup()
+if __name__=='__main__':
+    main()
