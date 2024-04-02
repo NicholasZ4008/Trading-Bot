c10b4a82d962ea12ef8ec0c7a72d510eca0330c2
nza34 <nza34@sfu.ca>
2024-04-01 17:36:06 -0700

    working on indicator.py and yahoofinance section

--- SMP500/main_smp500.py
+++ SMP500/main_smp500.py
@@ -1,9 +1,15 @@
 from web_scraper_SMP500 import scrape, format_raw
+from yahoofinance_SMP500 import getting_gaining_stocks_smp500
+from indicator import indicator_for_setup
 
 def main():
     raw = scrape()
     smp500_df=format_raw(raw)
     smp500_df.to_csv('dataset_smp500.csv', index=False)
+    getting_gaining_stocks_smp500()
+    indicator_for_setup()
+    
+    
     
 if __name__=='__main__':
     main()
--- SMP500/web_scraper_SMP500.py
+++ SMP500/web_scraper_SMP500.py
@@ -1,19 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import numpy as np
+import time
 
 def scrape():
-    page = []
-    my_url = 'https://www.slickcharts.com/sp500'
-    page.append(my_url)
-    
     values_list = []
+    #setup Selenium driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://www.slickcharts.com/sp500")
+    time.sleep(5) 
     
-    webpage = requests.get(page)
-    soup = bs(webpage.text , 'html.parser')
+    soup = bs(driver.page_source, 'html.parser')
+    stock_table = soup.find('table', class_='table table-hover table-borderless table-sm')
     
-    stock_table = soup.find('table', class_= 'table table-hover table-borderless table-sm')
     tr_tag_list = stock_table.find_all('tr')
     
     for each_tr_tag in tr_tag_list[1:]:
@@ -24,10 +26,12 @@ def scrape():
             new_value = each_td_tag.text.strip()
             row_values.append(new_value)
         values_list.append(row_values)
-     
+    
+    driver.quit()
     return(values_list)
 
 def format_raw(raw):
+    index = []
     company_name = []
     stock_symbol = []
     portfolio_percent = []
@@ -38,19 +42,22 @@ def format_raw(raw):
     for stock in raw:
         for i in range(len(stock)):
             if i == 0:
-                company_name.append(stock[i])
+                index.append(stock[i])
             elif i == 1:
-                stock_symbol.append(stock[i])
+                company_name.append(stock[i])
             elif i == 2:
-                portfolio_percent.append(stock[i])
+                stock_symbol.append(stock[i])
             elif i == 3:
-                price.append(stock[i])
+                portfolio_percent.append(stock[i])
             elif i == 4:
-                flat_change.append(stock[i])
+                price.append(stock[i])
             elif i == 5:
+                flat_change.append(stock[i])
+            elif i == 6:
                 percent_change.append(stock[i])
     
     smp500_df = pd.DataFrame({
+        'index' : index,
         'company_name': company_name,
         'stock_symbol': stock_symbol,
         'portfolio_percent': portfolio_percent,
