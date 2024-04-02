a9812ec56e0719edbf2f8fe3b7a5623523282790
nza34 <nza34@sfu.ca>
2024-04-01 22:57:03 -0700

    modifiying decisions

--- SMP500/smp500_decision.py
+++ SMP500/smp500_decision.py
@@ -3,9 +3,18 @@ import pandas as pd
 def decisions():
     # Load your dataframe containing historic stock data
     gaining_stocks_df = pd.read_csv('raw_yearly_yahoo_finance.csv')
+    
     # Assuming 'price' column represents the closing price of the stock
     # Calculate the 200-day moving average for each stock
-    gaining_stocks_df['200_MA'] = gaining_stocks_df.groupby('Symbol')['Close'].transform(lambda x: x.rolling(window=200).mean())
+    moving_avg_df = gaining_stocks_df.groupby('Symbol')['Close'].rolling(window=200).mean().reset_index(level=0, drop=True)
+    moving_avg_df = moving_avg_df.reset_index()  # Resetting the index
+    
+    # Rename the columns for clarity
+    moving_avg_df.rename(columns={'Close': '200_MA'}, inplace=True)
+    
+    # Merge the moving average back into the original dataframe
+    gaining_stocks_df = pd.merge(gaining_stocks_df, moving_avg_df, left_index=True, right_index=True, suffixes=('', '_MA'))
+    
     def determine_decision(row):
         if row['Close'] > row['200_MA']:
             return 'Raising'
